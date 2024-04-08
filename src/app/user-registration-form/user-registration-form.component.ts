a8f798d0ad2f2505c59a3c8c28d4c9da44a6ba01
lin <lin7cb@outlook.com>
2024-04-08 21:34:20 +0100

    Update user-registration-form.component.ts

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -33,13 +33,13 @@ export class UserRegistrationFormComponent implements OnInit {
         // Logic for a successful user registration goes here! (To be implemented)
         this.dialogRef.close(); // This will close the modal on success!
         console.log(result);
-        this.snackBar.open(result, 'OK', {
+        this.snackBar.open('User registration successful', 'OK', {
           duration: 2000,
         });
       },
       (result) => {
         console.log(result);
-        this.snackBar.open(result, 'OK', {
+        this.snackBar.open('User registration successful', 'OK', {
           duration: 2000,
         });
       }
